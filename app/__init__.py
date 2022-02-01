92679c3a9d0f8a44c330c1cfc0bf40a8119d420e
nancy nyokabi <nancynyokabi1995@gmail.com>
2022-02-01 09:57:13 +0300

    Added boootstrap and css to index.html

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,18 @@
+from distutils import extension
+from ensurepip import bootstrap
 from flask import Flask
 from config import devConfig
+from flask_bootstrap import Bootstrap
 
 #Initializing application
 app = Flask(__name__,instance_relative_config = True)
 
 #setting up the application
 app.config.from_object(devConfig)
+
 # app.config.from_pyfile('config.py')
+
+#initializing  flask extensions
+bootstrap = Bootstrap(app)
 
 from app import views
